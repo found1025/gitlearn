eec1883660f25120007a9ce4a85e62a434bb65ce
Liang <wangliang03@kuaishou.com>
2019-03-07 17:33:39 +0800

    try to modify

--- JavaPro.java
+++ JavaPro.java
@@ -4,7 +4,7 @@ public class JavaPro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is a test message!");
+		System.out.println("This is a test message!!!!!!");
 
 	}
 
